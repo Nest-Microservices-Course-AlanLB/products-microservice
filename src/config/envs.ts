f2d7ab97b0b6c05c6b4e1788583030c3064afb97
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-04-27 02:09:59 -0500

    Refactor - change from TCP to NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,19 +5,23 @@ import * as joi from 'joi';
 interface EnvVars {
     PORT: number;
     DATABASE_URL: string;
+    NATS_SERVERS: string[];
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
-})
-.unknown(true);
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+}).unknown(true);
 
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
-if ( error ) {
-    throw new Error(`Config validation error: ${ error.message }`);
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
 }
 
 const envVars: EnvVars = value;
@@ -25,4 +29,5 @@ const envVars: EnvVars = value;
 export const envs = {
     port: envVars.PORT,
     DATABASE_URL: envVars.DATABASE_URL,
+    natsServers: envVars.NATS_SERVERS,
 }
